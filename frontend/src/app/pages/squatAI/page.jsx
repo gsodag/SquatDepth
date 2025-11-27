64b28db75252cb538a0c1ff03d40d812c31dcc8e
piotr <piotr@LAPTOP-JAPFURLK>
2025-11-27 20:26:32 +0100

    upload form

--- frontend/src/app/pages/squatAI/page.jsx
+++ frontend/src/app/pages/squatAI/page.jsx
@@ -1,10 +1,11 @@
 import Header from "@/app/components/header/Header";
- 
+import UploadForm from "@/app/components/uploadForm/UploadForm"; 
+
 export default function Home() {
   return (
     <div >
       <Header></Header>
-      
+      <UploadForm></UploadForm>
     </div>
   );
 }
